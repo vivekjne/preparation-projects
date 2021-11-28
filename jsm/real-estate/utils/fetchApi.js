d951ff50da356327c6df0c54224193c867979d28
Vivek Joy <joyvivek93@gmail.com>
2021-11-28 21:05:58 +0530

    added env file

--- jsm/real-estate/utils/fetchApi.js
+++ jsm/real-estate/utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "620178282cmsh94312924cdd4e03p19b208jsn4a72fda18899",
+      "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
     },
   });
   return data;
